d7356811b17985f7b962936ce20d3fa148558ac1
CutoNaito <loliknightowo@gmail.com>
2022-09-28 21:08:02 +0200

    reactaopp WQOOOO

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,13 @@
-export async function testquery(connection) {
-    return await connection.execute('SELECT * FROM test');
+export async function selectusers(connection) {
+    return await connection.execute('SELECT * FROM users');
 }
+
+export async function selectproducts(connection) {
+    return await connection.execute('SELECT * FROM zbozi');
+}
+
+export async function selectorders(connection) {
+    return await connection.execute('SELECT * FROM objednavky');
+}
+
 
---
+++ frontend/src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <img src="" alt="kuskusdeda-logo"/>
+            <h1>Kuskus Deda</h1>
+        </header>
+    )
+}
+
+export default Header
